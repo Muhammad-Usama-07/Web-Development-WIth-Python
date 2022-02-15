e75b2c95889890a17893b148bde63bc4f49a858c
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2022-02-15 12:01:28 +0500

    Sending data
    Using GET to send data

--- Flask-API/api.py
+++ Flask-API/api.py
@@ -1,11 +1,29 @@
 import flask
+from flask import request, jsonify
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+detail = [
+    {
+        'name': 'API',
+        'lastname': 'work'
+    },
+    {
+        'name': 'API2',
+        'lastname': 'work2'
+    }
+]
+
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Voice Attendence System</h1>"
+    return '''API Work</h1>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/books/all', methods=['GET'])
+def api_all():
+    return jsonify(detail)
 
 app.run()
