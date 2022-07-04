e6674e2bb2df08a5605b5dfdf5880f3b5ee72ef8
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2022-07-04 17:45:31 +0500

    adding student class
    a class contain id name and contact

--- Django_CRUD_Application/CRUD/models.py
+++ Django_CRUD_Application/CRUD/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    sid = models.CharField(max_length=4)
+    sname = models.CharField(max_length=255)
+    scontact = models.CharField(max_length=15)
+    def __str__(self):
+        return self.sname
+
