61283f3d95918f8c64504f4e0e43b2f6f8a8579b
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2022-07-04 18:21:54 +0500

    adding views of CRUD

--- Django_CRUD_Application/mysite/urls.py
+++ Django_CRUD_Application/mysite/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from modulefinder import IMPORT_NAME
 from django.contrib import admin
 from django.urls import path
+from CRUD import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.add, name = 'add')
 ]
