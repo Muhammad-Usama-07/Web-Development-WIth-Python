91eec89963c1bf40f78dd99776c658c641785a26
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2022-07-05 13:25:13 +0500

    Working with CSS

--- Django_CRUD_Application/mysite/settings.py
+++ Django_CRUD_Application/mysite/settings.py
@@ -115,10 +115,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+import os
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = ['/CRUD/static/', os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [
+#     # '/firstapp/static/css'
+#     '../static/'
+#     # 'CRUD/static/'
+# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
