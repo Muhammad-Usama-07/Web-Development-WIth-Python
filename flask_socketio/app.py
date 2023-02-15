585e0960bb9c2d38e88f836f5fbadc2d2b37e554
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2023-02-15 13:36:20 +0500

    run flask app using socket

--- flask_socketio/app.py
+++ flask_socketio/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 app = Flask(__name__)
+socket = SocketIO(app)
 li = [1,2,3,4,5]
 @app.route('/')
 def main():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socket.run(app)
 
