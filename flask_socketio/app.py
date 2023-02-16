8483460e0a055b32bdb994ab104277a89f56d9be
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2023-02-16 11:47:21 +0500

    send list to console using socket io

--- flask_socketio/app.py
+++ flask_socketio/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+import time
 app = Flask(__name__)
 socket = SocketIO(app)
 li = [1,2,3,4,5]
+i=0
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@socket.on('message')
+def handlemss(msg):
+    global i
+    if i < len(li):
+        time.sleep(10)
+        socket.send(li[i])
+        i+=1
 
 if __name__ == "__main__":
     socket.run(app)
